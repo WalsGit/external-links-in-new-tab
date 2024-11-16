7880d2067cd253e56bc0b612c7cc636e0be87287
Wa!id <108340184+WalsGit@users.noreply.github.com>
2024-11-16 14:45:32 +0100

    initial release

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Changelog
+
+## [1.0.0] - 2024-11-16
+- Open external links of the forum frontend in a new tab
--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -7,12 +7,10 @@ import DiscussionPage from 'flarum/forum/components/DiscussionPage';
 import Post from 'flarum/forum/components/Post';
 
 app.initializers.add('walsgit/external-links', () => {
-    // Fonction pour ouvrir les liens externes dans un nouvel onglet
     const openExternalLinksInNewTab = () => {
         document.querySelectorAll('a[href^="http"]').forEach((element: Element) => {
             if (element instanceof HTMLAnchorElement) {
                 const link = element;
-                // Vérifier si le lien est externe
                 if (!link.href.includes(window.location.hostname)) {
                     link.setAttribute('target', '_blank');
                     link.setAttribute('rel', 'noopener noreferrer');
@@ -21,28 +19,27 @@ app.initializers.add('walsgit/external-links', () => {
         });
     };
 
-    // Exclure la section admin
     if (!window.location.pathname.startsWith('/admin')) {
-        // Cibler les liens de navigation et de menu
+        // Target nav and menu links
         extend(IndexPage.prototype, 'oncreate', openExternalLinksInNewTab);
         extend(DiscussionPage.prototype, 'oncreate', openExternalLinksInNewTab);
 
-        // Cibler les liens dans les posts
+        // Target post links
         extend(PostStream.prototype, 'oncreate', function () {
             openExternalLinksInNewTab();
         });
 
-        // Cibler tout autre lien présent après le rendu de la page
+        // Target all other extarnal links in the page
         openExternalLinksInNewTab();
 
-        // Écouter l'événement de création de post
+        // Listen to post creation event
         extend(Post.prototype, 'oncreate', function () {
-            openExternalLinksInNewTab(); // Appeler la fonction après la création du post
+            openExternalLinksInNewTab();
         });
 
-        // Écouter l'événement de mise à jour de post
+        // Listen to post update event
         extend(Post.prototype, 'onupdate', function () {
-            openExternalLinksInNewTab(); // Appeler la fonction après la mise à jour du post
+            openExternalLinksInNewTab();
         });
     }
 });
